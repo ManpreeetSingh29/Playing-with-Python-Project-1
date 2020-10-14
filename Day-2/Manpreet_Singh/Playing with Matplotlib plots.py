91572113357f90b5cd0ce17beb20c6050d8157ab
ManpreeetSingh29 <72621588+ManpreeetSingh29@users.noreply.github.com>
2020-10-14 18:28:21 +0530

    Update Playing with Matplotlib plots.py

--- Day-2/Manpreet_Singh/Playing with Matplotlib plots.py
+++ Day-2/Manpreet_Singh/Playing with Matplotlib plots.py
@@ -20,13 +20,13 @@ playing = [8, 5, 7, 8, 13]
 slices = [7, 2, 2, 13, 4]
 gaming = [9, 7, 6, 8, 3]
 activities = ['sleeping','eating','working','playing','gaming']
-cols = ['c','m','r','b','y']
+cols = ['c', 'm', 'r', 'b', 'y']
  
-plt.pie(slices,
-labels=activities,
-colors=cols,
-startangle=90,shadow= True,
-explode = (0, 0.1, 0, 0, 0),
+plt.pie(slices, 
+labels=activities, 
+colors=cols, 
+startangle=90,shadow= True, 
+explode = (0, 0.1, 0, 0, 0), 
 autopct='%1.1f%%')
 
 plt.title('Pie Plot')
